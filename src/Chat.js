47a93f717c5852de6c2ce55f11bb40fb0f52ead6
SrinivasaLakshman <srinivas.1718ccd@gmail.com>
2023-10-21 22:36:57 +0530

    chat component

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,72 @@
 import React from 'react'
 import "./Chat.css";
+import { Avatar, IconButton } from '@mui/material';
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined, Mic } from '@mui/icons-material';
+import { useState } from 'react';
 
 function Chat() {
+
+  const {input, setInput} = useState("");
+
+  const sendMessage = () => {
+    console.log(`This is message ${input}`);
+  }
+
   return (
-    <div className='Chat'>
-        <h1>i am the Chat</h1>
+    <div className='chat'>
+      <div className='chat_header'>
+        <Avatar/>
+
+        <div className='chat_headerInfo'>
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+
+        <div className='chat_headerRight'>
+          <IconButton>
+            <SearchOutlined/>
+          </IconButton>
+          <IconButton>
+            <AttachFile/>
+          </IconButton>
+          <IconButton>
+            <MoreVert/>
+          </IconButton>
+        </div>
+      </div>
+
+      <div className='chat_body'>
+        <p className='chat_message'>
+          <span className='chat_name'>Srinivas</span>
+          This is a message
+          <span className='chat_timestamp'>{new
+          Date().toUTCString()}</span>
+        </p>
+
+        <p className='chat_message chat_reciever'>
+          <span className='chat_name'>Srinivas</span>
+          This is a message
+          <span className='chat_timestamp'>{new
+          Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className='chat_footer'>
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder='Type a message'
+            type='text'
+          />
+          <button onClick={sendMessage}
+          type='submit'>
+            sendMessage
+          </button>
+        </form>
+        <Mic/>
+      </div>
     </div>
   )
 }
