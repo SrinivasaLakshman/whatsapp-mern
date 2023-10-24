5cd8fa1dbc758c2e8821f10e27b52ddbe104291a
SrinivasaLakshman <srinivas.1718ccd@gmail.com>
2023-10-24 12:53:40 +0530

    pusher js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('e0240fc6d04ca0fa9821', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
   return (
     <div className="app">
       <div className="app_body">
